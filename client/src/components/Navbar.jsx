5ad9a61c0dc0bf4c6185e75c68ed4c15f795b953
danhcodelabs <danhcodelabs@gmail.com>
2025-07-27 17:56:07 +0700

    cấu hình lại tailwind

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { assets } from "../assets/assets";
-
 const Navbar = () => {
   return (
-    <div className="bg-white w-full flex items-center justify-center py-2">
+    <div className="bg-white w-full flex items-center justify-center py-4">
       <div className="w-[1400px]  mx-auto">
         <div className="flex flex-col items-center">
           <div className="flex items-center justify-between w-full">
             <div>
               <img src={assets.logo} alt="" />
+            </div>
+            <div className="flex flex-1 justify-center items-center gap-2">
+              <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+              </div>
             </div>
           </div>
         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-red-500'>Home</div>
   )
 }
 
-export default Home 
+export default Home
